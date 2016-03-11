a83e33715fc488d0b462c4376cac79539c03c254
David Podhola <david@podhola.net>
2016-03-11 11:07:43 +0100

    initial commit

---
+++ tests/fs2cs.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module fs2cs.Tests
+
+open fs2cs
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
