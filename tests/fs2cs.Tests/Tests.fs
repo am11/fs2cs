2c444a20cec16a1cf79ac6e3a89b2be12aa515a9
David Podhola <david.podhola@naseukoly.cz>
2016-03-11 15:45:53 +0100

    OK now we can have a look into the compiled file

--- tests/fs2cs.Tests/Tests.fs
+++ tests/fs2cs.Tests/Tests.fs
@@ -3,6 +3,7 @@ module fs2cs.Tests
 open fs2cs
 open NUnit.Framework
 open Serilog
+open Microsoft.FSharp.Compiler.SourceCodeServices
 
 [<SetUpFixture>]
 type SetupTest() =
@@ -22,8 +23,42 @@ let ``hello returns 42`` () =
   Assert.AreEqual(42,result)
 
 [<Test>]
-let ``empty () compile works`` () =
+let ``empty "()" compile works`` () =
   let compiled = Library.main [|"--code";"()"|]
   //Log.Debug( "{@Compiled}", compiled )
   Assert.NotNull(compiled)
   Assert.IsEmpty(compiled.Errors)
+
+[<Test>]
+let ``simple "let a=12345" compile works`` () =
+  let compiled = Library.main [|"--code";"let a=12345"|]
+  //Log.Debug( "{@Compiled}", compiled )
+  Assert.NotNull(compiled)
+  Assert.IsEmpty(compiled.Errors)
+  Assert.IsNotEmpty( compiled.AssemblyContents.ImplementationFiles )
+  let declarations = compiled.AssemblyContents.ImplementationFiles.Head.Declarations
+  Assert.IsNotEmpty( declarations )
+  let declaration = declarations.Head
+  match declaration with
+  | Entity(x,y) -> 
+      match y.Head with 
+      | MemberOrFunctionOrValue(a, b, c) -> Assert.AreEqual("a",a.DisplayName)
+      | _ -> Assert.Fail(sprintf "OTHER: %A" y)
+  | _ -> Assert.Fail(sprintf "OTHER: %A" declaration)
+
+[<Test>]
+let ``simple "printfn \"Hello\"" compile works`` () =
+  let compiled = Library.main [|"--code";"printfn \"Hello\""|]
+  Assert.NotNull(compiled)
+  Assert.IsEmpty(compiled.Errors)
+  Assert.IsNotEmpty( compiled.AssemblyContents.ImplementationFiles )
+  let declarations = compiled.AssemblyContents.ImplementationFiles.Head.Declarations
+  Assert.IsNotEmpty( declarations )
+  let declaration = declarations.Head
+  match declaration with
+  | Entity(x,y) -> 
+      match y.Head with 
+      | MemberOrFunctionOrValue(a, b, c) -> Assert.AreEqual("a",a.DisplayName)
+      | _ -> Assert.Fail(sprintf "OTHER: %A" y)
+  | _ -> Assert.Fail(sprintf "OTHER: %A" declaration)
+  Log.Debug( "{@declarations}", declarations )  
