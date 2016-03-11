3cf74f0d319958957599f9a6194e49b4867689df
David Podhola <david.podhola@naseukoly.cz>
2016-03-11 11:44:06 +0100

    code copied from Fable

--- tests/fs2cs.Tests/Tests.fs
+++ tests/fs2cs.Tests/Tests.fs
@@ -8,3 +8,9 @@ let ``hello returns 42`` () =
   let result = Library.hello 42
   printfn "%i" result
   Assert.AreEqual(42,result)
+
+[<Test>]
+let ``empty compile works`` () =
+  let result = Library.main 
+  printfn "%A" result
+  Assert.NotNull(result)
