ff328067d10772e11938d9e7724ed5a3f85c049e
Zaneta Filipovska <leia@bofh.cz>
2016-03-21 14:57:52 +0100

    script for viewing abstract syntax tree

---
+++ src/fs2cs/ASTViewer.fsx
@@ -0,0 +1,50 @@
+﻿#r "../../packages/build/FSharp.Compiler.Service/lib/net45/FSharp.Compiler.Service.dll"
+#r "../../packages/build/FSharp.Compiler.Service.ProjectCracker/lib/net45/FSharp.Compiler.Service.ProjectCracker.dll"
+
+open System
+open System.IO
+open Microsoft.FSharp.Compiler
+open Microsoft.FSharp.Compiler.Ast
+open Microsoft.FSharp.Compiler.SourceCodeServices
+open Microsoft.FSharp.Compiler.SourceCodeServices.BasicPatterns
+
+let checker = FSharpChecker.Create(keepAssemblyContents=true)
+
+let parse projFile =
+    let options =
+        match Path.GetExtension(projFile) with
+        | ".fsx" ->
+            let projCode = File.ReadAllText projFile
+            checker.GetProjectOptionsFromScript(projFile, projCode)
+            |> Async.RunSynchronously
+        | ".fsproj" ->
+            ProjectCracker.GetProjectOptionsFromProjectFile(Path.GetFullPath projFile)
+        | ext -> failwithf "Unexpected extension: %s" ext
+    options
+    |> checker.ParseAndCheckProject
+    |> Async.RunSynchronously
+    
+let rec printDecls prefix decls =
+    decls |> Seq.iteri (fun i decl ->
+        match decl with
+          | FSharpImplementationFileDeclaration.Entity (e, sub) ->
+              printfn "%s%i) ENTITY: %s" prefix i e.DisplayName
+              printDecls (prefix + "\t") sub
+          | FSharpImplementationFileDeclaration.MemberOrFunctionOrValue (meth, args, body) ->
+              if meth.IsCompilerGenerated |> not then
+                  printfn "%s%i) METHOD: %s" prefix i meth.DisplayName
+                  // match body with
+                  // | NewDelegate(_, Lambda(arg1, Lambda(arg2, _))) ->
+                  //     printfn "arg1 = arg2 %A" (arg1.IsCompilerGenerated)
+                  // | _ -> ()
+                  printfn "%A" body
+          | FSharpImplementationFileDeclaration.InitAction (expr) ->
+              printfn "%s%i) ACTION" prefix i
+              printfn "%A" expr
+        )
+
+let proj = parse "temp/test.fsx"
+proj.AssemblyContents.ImplementationFiles
+|> Seq.iteri (fun i file -> printfn "%i) %s" i file.FileName)
+proj.AssemblyContents.ImplementationFiles.[0].Declarations
+|> printDecls ""
