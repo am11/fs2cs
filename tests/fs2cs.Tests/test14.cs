536796499078c305f253afe133b66223db90c304
Zaneta Filipovska <leia@bofh.cz>
2016-03-23 16:30:25 +0100

    test 14 added

---
+++ tests/fs2cs.Tests/test14.cs
@@ -0,0 +1,17 @@
+﻿public class Test14
+{
+    public static int mul(int m, int n, int o)
+    {
+        return m * n * o;
+    }
+
+    public static int y(Func<dynamic, dynamic, dynamic, dynamic> funkce, dynamic a, dynamic b, dynamic c)
+    {
+        return funkce(a, b, c) + 1;
+    }
+
+    public static int Invoke()
+    {
+        return y(mul, 2, 3, 4);
+    }
+}
